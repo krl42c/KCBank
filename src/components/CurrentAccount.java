df82711ecb47fd46386fe4b82f415577d08649a5
krlc9 <karolc9991@gmail.com>
2022-10-05 11:35:33 +0200

    fixed constructor vis

--- src/components/CurrentAccount.java
+++ src/components/CurrentAccount.java
@@ -3,7 +3,7 @@ package components;
 // 1.2.2 Creation of the CurrentAccount and SavingsAccount
 public class CurrentAccount extends Account {
 	
-	CurrentAccount(String label, Client client) {
+	public CurrentAccount(String label, Client client) {
 		super(label, client);
 	}
 	
--- src/components/SavingsAccount.java
+++ src/components/SavingsAccount.java
@@ -3,7 +3,7 @@ package components;
 // 1.2.2 Creation of the CurrentAccount and SavingsAccount
 public class SavingsAccount extends Account {
 
-	SavingsAccount(String label, Client client) {
+	public SavingsAccount(String label, Client client) {
 		super(label, client);
 	}
 
