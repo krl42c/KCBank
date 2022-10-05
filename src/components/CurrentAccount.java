cb9abf6fb72b47b5c7fbd0f3c22c317c1acab527
krlc9 <karolc9991@gmail.com>
2022-10-05 11:24:15 +0200

    created CurrentAccount

---
+++ src/components/CurrentAccount.java
@@ -0,0 +1,10 @@
+package components;
+
+// 1.2.2 Creation of the CurrentAccount and SavingsAccount
+public class CurrentAccount extends Account {
+	
+	CurrentAccount(String label, Client client) {
+		super(label, client);
+	}
+	
+}
